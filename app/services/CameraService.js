79d877872b1e94a67a79d9ef0e722415fcbaef07
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2015-11-21 10:39:13 +0100

    feat: make CameraService call backend

--- app/services/CameraService.js
+++ app/services/CameraService.js
@@ -1,7 +1,52 @@
+import Promise from 'bluebird';
+import superagent from 'superagent';
+import _ from 'lodash';
+
+Promise.promisifyAll(superagent);
+
+const Method = {
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DELETE: 'del'
+};
+
+function request(options) {
+  return new Promise((resolve, reject) => {
+    options = _.defaults(options || {}, {
+      type: 'json',
+      method: Method.GET,
+      contentType: 'application/json'
+    });
+
+    let reqObj = superagent[options.method](options.url);
+    if (typeof options.data !== 'undefined') {
+      reqObj = reqObj.send(options.data);
+    } else {
+      reqObj = reqObj.send('');
+    }
+
+    reqObj
+      .set('Content-Type', options.contentType)
+      .set('Accept', 'application/json')
+      .endAsync()
+      .then(response => {
+        resolve(response.body);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
 export default {
 
   takePicture () {
-    console.log('flash');
+    return request({
+      url: 'camera/takePicture'
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
 };
