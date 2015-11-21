ed1f72b06fe24249f68b8a7fa5a1a30292a0ef24
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2015-11-21 09:51:36 +0100

    chore: disable seed components

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -3,37 +3,35 @@ import styles from './_App.scss';
 import React from 'react';
 import AppActions from '../../actions/AppActions';
 import ItemsStore from '../../stores/ItemsStore';
-import Body from '../Body/Body';
-import Footer from '../Footer/Footer';
 
+/*
 function getAppState() {
   return {
     items: ItemsStore.getAll()
   };
 }
+*/
 
 export default class App extends React.Component {
 
-  state = getAppState()
+  //state = getAppState()
 
   componentDidMount() {
-    ItemsStore.addChangeListener(this.onChange);
-    AppActions.getItems();
+    //ItemsStore.addChangeListener(this.onChange);
+    //AppActions.getItems();
   }
 
   componentWillUnmount() {
-    ItemsStore.removeChangeListener(this.onChange);
+    //ItemsStore.removeChangeListener(this.onChange);
   }
 
   onChange = () => {
-    this.setState(getAppState());
+    //this.setState(getAppState());
   }
 
   render() {
     return (
       <div className={styles.app}>
-        <Body items={this.state.items} />
-        <Footer />
       </div>
     );
   }
